a97982ef68e8bc308c7d7fd8758657e8bd097ec6
Sergiy Popovych <sergiy.popovich@gmail.com>
2019-05-20 11:54:01 -0400

    instancenorm experiments

--- params/05_17_instnorm_experiment_create.sh
+++ params/05_17_instnorm_experiment_create.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-python3 create_categorical.py --compch 3 5 15   --outch 2 --grid true --maxvalue 3 7 --trainst false true --step null
-python3 create_m4_categorical.py --compch 3 5 15   --outch 2 --grid true --maxvalue 3 7 --trainst false true --step null
-python3 create_experiment.py --compch 3 5 15  --outch 2 --grid true --maxvalue 3 7 --trainst false true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_noaugment.json" --model m4_categorical --name_flag noaug
-python3 create_experiment.py --compch 3 5 15  --outch 2 --grid true --maxvalue 3 7 --trainst false true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_augment02.json" --model m4_categorical --name_flag aug02
+python3 create_categorical.py --compch 5 --outch 2 --grid true --maxvalue 7 --trainst true --step null
+python3 create_architecture.py --name instnorm_categorical  --compch 5 --outch 2 --grid true --maxvalue 7 --trainst true --step null
+python3 create_architecture.py --name m4_categorical  --compch 5 --outch 2 --grid true --maxvalue 7 --trainst true --step null
+
+python3 create_categorical.py --compch 15 --outch 2 --grid false --maxvalue 7 --trainst true --step null
+python3 create_architecture.py --name instnorm_categorical  --compch 15 --outch 2 --grid false --maxvalue 7 --trainst true --step null
+python3 create_architecture.py --name m4_categorical  --compch 15 --outch 2 --grid false --maxvalue 7 --trainst true --step null
+
+python3 create_experiment.py --compch 5 --outch 2 --grid true --maxvalue 7 --trainst true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_noaugment.json" --model instnorm_categorical --name_flag noaug
+python3 create_experiment.py --compch 15 --outch 2 --grid false --maxvalue 7 --trainst true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_noaugment.json" --model instnorm_categorical --name_flag noaug
+
+python3 create_experiment.py --compch 5 --outch 2 --grid true --maxvalue 7 --trainst true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_noaugment.json" --model m4_categorical --name_flag noaug
+python3 create_experiment.py --compch 15 --outch 2 --grid false --maxvalue 7 --trainst true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_noaugment.json" --model m4_categorical --name_flag noaug
