951afd3765f6ad76414b768bf2a2a9f6d000fae3
Sergiy Popovych <sergiy.popovich@gmail.com>
2019-06-26 18:38:19 -0400

    update

--- params/05_20_experiment_create.sh
+++ params/05_20_experiment_create.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-python3 create_categorical.py --compch 3 5 15   --outch 2 --grid true --maxvalue 3 7 --trainst false true --step null
-python3 create_m4_categorical.py --compch 3 5 15   --outch 2 --grid true --maxvalue 3 7 --trainst false true --step null
-python3 create_experiment.py --compch 3 5 15  --outch 2 --grid true --maxvalue 3 7 --trainst false true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_noaugment.json" --model m4_categorical --name_flag noaug
-python3 create_experiment.py --compch 3 5 15  --outch 2 --grid true --maxvalue 3 7 --trainst false true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_augment02.json" --model m4_categorical --name_flag aug02
+python3 create_categorical.py --compch 3 9 15 21 45   --outch 2 --grid false --maxvalue 7 --trainst true --step null
+python3 create_categorical.py --compch 3 5 7 9 11  --outch 2 --grid true --maxvalue 7 --trainst true --step null
+
+python3 create_m4_categorical.py --compch 3 5 7 9 11  --outch 2 --grid true --maxvalue 7 --trainst true --step null
+python3 create_m4_categorical.py --compch 3 9 15 21 45   --outch 2 --grid false --maxvalue 7 --trainst true --step null
+
+python3 create_experiment.py --compch 3 5 7 9 11 --outch 2 --grid true --maxvalue 7 --trainst true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_noaugment.json" --model m4_categorical --name_flag noaug
+python3 create_experiment.py --compch 3 5 7 9 11 --outch 2 --grid true --maxvalue 7 --trainst true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_augment02.json" --model m4_categorical --name_flag aug02
+
+python3 create_experiment.py --compch 3 9 15 21 45 --outch 2 --grid false --maxvalue 7 --trainst true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_noaugment.json" --model m4_categorical --name_flag noaug
+python3 create_experiment.py --compch 3 9 15 21 45 --outch 2 --grid false --maxvalue 7 --trainst true --step null --datapath "/tigress/popovych/aligner/params/data/data_params_m2_augment02.json" --model m4_categorical --name_flag aug02
